4152f2863b0ab28358f4d223c92c589461c6c88c
Sakshi Jashnani <sakshijashnani50@gmail.com>
2025-02-12 12:14:36 +0000

    Added EndTurnClicked event processor logic and created AIController class

--- app/events/Initalize.java
+++ app/events/Initalize.java
@@ -29,7 +29,7 @@ public class Initalize implements EventProcessor{
 		gameState.something = true;
 		
 		// User 1 makes a change
-		CommandDemo.executeDemo(out); // this executes the command demo, comment out this when implementing your solution
+		//CommandDemo.executeDemo(out); // this executes the command demo, comment out this when implementing your solution
 		//Loaders_2024_Check.test(out);
 	}
 
--- app/structures/GameState.java
+++ app/structures/GameState.java
@@ -1,5 +1,7 @@
 package structures;
 
+import structures.basic.Player;
+
 /**
  * This class can be used to hold information about the on-going game.
  * Its created with the GameActor.
@@ -8,10 +10,43 @@ package structures;
  *
  */
 public class GameState {
+    private int currentTurn;  // Current turn number
+    private Player currentPlayer;  // Player whose turn it is
+    private Player opponentPlayer;  // The opponent player
 
-	
-	public boolean gameInitalised = false;
-	
-	public boolean something = false;
-	
+    public GameState() {
+        this.currentTurn = 1;  // Start the game on turn 1
+        this.currentPlayer = null;  // Initialize event handler to set up
+        this.opponentPlayer = null;  // Initialize event handler to set up
+    }
+
+    // Method to initialize the players for the first time
+    public void initializePlayers(Player player1, Player player2) {
+        this.currentPlayer = player1;  // Set the current player
+        this.opponentPlayer = player2;  // Set the opponent player
+    }
+
+    public void nextTurn() {
+        this.currentTurn++;  // Increment the turn number
+
+        // Swap current player and opponent player
+        Player temp = this.currentPlayer;
+        this.currentPlayer = this.opponentPlayer;
+        this.opponentPlayer = temp;
+    }
+
+    // Getter for the current turn number
+    public int getCurrentTurn() {
+        return currentTurn;
+    }
+
+    // Getter for the current player
+    public Player getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    // Getter for the opponent player
+    public Player getOpponentPlayer() {
+        return opponentPlayer;
+    }
 }
