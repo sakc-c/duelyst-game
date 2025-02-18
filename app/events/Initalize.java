02156b1103aadebc88a8e4fdf8d6ebdbebf7d49f
“bhumiii10” <2994327a@student.gla.ac.uk>
2025-02-18 14:43:59 +0000

    Solving issues with last merge

--- app/events/Initalize.java
+++ app/events/Initalize.java
@@ -6,11 +6,8 @@ import akka.actor.ActorRef;
 import demo.CommandDemo;
 import demo.Loaders_2024_Check;
 import structures.GameState;
-<<<<<<< HEAD
 import structures.basic.Board;
-=======
 import commands.BasicCommands;
->>>>>>> origin/main
 
 /**
  * Indicates that both the core game loop in the browser is starting, meaning
@@ -36,18 +33,35 @@ public class Initalize implements EventProcessor{
 		Board board = new Board(out); // initalizing the board
 		gameState.setBoard(board); // Store the board in GameState
 
-		
-		Player aiPlayer = gameState.getAIPlayer();
+		// Initialise Players
+	    gameState.player1 = new HumanPlayer(20, 2);
+	    gameState.player2 = new AIController(20, 2);
 
-        aiPlayer.setHealth(20); 
+	    // Initialise hands
+	    gameState.player1Hand = new ArrayList<>();
+	    gameState.player2Hand = new ArrayList<>();
 
-        // Use BasicCommands to display the AI player's health on the UI
-        BasicCommands.setPlayer2Health(out, aiPlayer);
-		
+	    // Set Player 1 & 2 health
+	    BasicCommands.setPlayer1Health(out, gameState.player1);
+	    BasicCommands.setPlayer2Health(out, gameState.player2);
+
+	    // Draw initial hands
+	    drawInitialHands(out, gameState);
 
 		// User 1 makes a change
 		//CommandDemo.executeDemo(out); // this executes the command demo, comment out this when implementing your solution
 		//Loaders_2024_Check.test(out);
+	}
+	
+	private void drawInitialHands(ActorRef out, GameState gameState) {
+	    for (int i = 0; i < 3; i++) {
+	        drawCardForPlayer(out, gameState, gameState.player1); // Initialise the hands of the HumanPlayer with 3 cards
+	        drawCardForPlayer(out, gameState, gameState.player2); // Initialise the hands of the AIController with 3 cards
+	    }
+	}
+
+	private void drawCardForPlayer(ActorRef out, GameState gameState, Player player) {
+	    player.drawCard(out); // Calls the drawCard() method on the HumanPlayer and AIController classes
 	}
 
 }
